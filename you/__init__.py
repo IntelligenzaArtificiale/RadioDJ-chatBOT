0c52877ff127e0e6c707bda8f7695d42886fd53c
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-04-11 17:07:19 +0200

    Update __init__.py

--- you/__init__.py
+++ you/__init__.py
@@ -22,7 +22,7 @@ class Completion:
         debug           : bool = False ) -> dict:
         
         client         = Session(client_identifier="chrome110")
-        client.cookies.clear()
+        client.cookies.set("safesearch_guest", "Moderate")
         client.headers = {
             "authority"         : "you.com",
             "accept"            : "text/event-stream",
