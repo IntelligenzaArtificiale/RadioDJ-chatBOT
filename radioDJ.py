b30067632d082bfbb89253f4d8bc566642eb0e85
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-04-11 17:59:33 +0200

    Update radioDJ.py

--- radioDJ.py
+++ radioDJ.py
@@ -19,9 +19,9 @@ import writesonic
 with open('style.css')as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
-img_html = " <center><img src='https://static.wixstatic.com/media/3c029f_7bb91a64433b4422aa475a7f200a63db~mv2.png'  height='200px' width='370px'></center> "
+img_html = " <center><img src='https://static.wixstatic.com/media/3c029f_7bb91a64433b4422aa475a7f200a63db~mv2.png'  height='200px' width='390px'></center> "
 st.markdown(img_html, unsafe_allow_html=True)
-
+sr.write(" ")
 if 'bot' not in st.session_state:
         st.session_state['user'] = []
         st.session_state['bot'] = []
